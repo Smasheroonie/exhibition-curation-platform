f4767b3855fe0f0e6692b9037cd0ddc1fcca11b1
smasheroonie <68129402+Smasheroonie@users.noreply.github.com>
2025-06-02 17:18:45 +0100

    minor card change

--- src/components/ArtworkCard.tsx
+++ src/components/ArtworkCard.tsx
@@ -6,14 +6,16 @@ type ArtworkCardProps = {
 export default function ArtworkCard({ title, thumbnail }: ArtworkCardProps) {
   return (
     <div className="flex gap-2 outline m-3 p-4">
-      <img
-        className="size-44 md:size-60 object-cover"
-        src={
-          thumbnail ||
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
-        }
-        alt="placeholder artwork"
-      />
+      <div className="outline">
+        <img
+          className="size-44 md:size-60 object-cover"
+          src={
+            thumbnail ||
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
+          }
+          alt="placeholder artwork"
+        />
+      </div>
       <div className="flex flex-col justify-between w-full">
         <div className="flex flex-col gap-1">
           <h2 className="font-bold">{title}</h2>
